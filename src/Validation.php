8849ced6f0fd25e72fb1cad7ce1e28704d169445
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-04-22 15:40:13 +0100

    Update Validation.php

--- src/Validation.php
+++ src/Validation.php
@@ -6,10 +6,17 @@
 // | @date          : 20 Apr, 2023 8:30 AM
 // +------------------------------------------------------------------------+
 
-namespace LogadApp\Validator;
+namespace Logadapp\Validator;
 
-class Validation {
-    private $errors = [];
+use Exception;
+
+class Validation
+{
+    public array $postData;
+    public array $files;
+    public array $rules;
+
+    private array $errors = [];
 
     public function getErrors(): array
     {
@@ -21,8 +28,122 @@ class Validation {
         return $this->errors[0];
     }
 
-    public function validate(array $post, array $files, array $rules)
+    public function make(array $post, array $files, array $rules):self
     {
-        
+        $this->postData = $post;
+        $this->files = $files;
+        $this->rules = $rules;
+        return $this;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function validate(array $post = [], array $files = [], array $rules = []): self
+    {
+        // Make method has not already been used
+        if (isset($rules) && !isset($this->rules)) {
+            $this->make($post, $files, $rules);
+        }
+
+        foreach ($this->rules as $fieldName => $ruleset) {
+            if (empty($this->postData[$fieldName])) {
+                $this->errors[$fieldName][] = 'required';
+                continue;
+            }
+
+            $ruleSets = explode('|', $ruleset);
+            print_r($ruleSets);
+            foreach ($ruleSets as $rule) {
+                $this->validateRule($rule, $fieldName, $this->postData[$fieldName], $this->files[$fieldName] ?? []);
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function validateRule(string $rule, string $field, mixed $value, mixed $file): void
+    {
+        $params = [];
+        $callback = null;
+
+        if (str_contains($rule, ':')) {
+            list($rule, $params) = explode(':', $rule, 2);
+            $params = explode(',', $params);
+        }
+
+        $methodName = 'validate' . ucfirst($rule);
+        echo $methodName, PHP_EOL;
+        if (method_exists($this, $methodName)) {
+            $callback = [$this, $methodName];
+        } elseif (function_exists($methodName)) {
+            $callback = $methodName;
+        }
+
+        if (is_callable($callback)) {
+            if (call_user_func($callback, $field, $value, $file, $params) === false) {
+                $this->errors[$field][] = $rule;
+            }
+        } else {
+            throw new Exception("Validation rule not found: $rule");
+        }
+    }
+
+    protected function validateRequired(string $field, string $value)
+    {
+        return !empty($value);
+    }
+
+    protected function validateMax(string $field, string|array $value, array $file, array $params): bool
+    {
+        $maxSize = (int) $params[0];
+
+        if (isset($file['size'])) {
+            return $file['size'] <= $maxSize;
+        }
+
+        if (is_string($value)) {
+            return strlen($value) <= $maxSize;
+        }
+
+        if (is_array($value)) {
+            return count($value) <= $maxSize;
+        }
+
+        return false;
+    }
+
+    protected function validateEmail(string $value): bool
+    {
+        return filter_var($value, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    protected function validateNumeric(mixed $value): bool
+    {
+        return is_numeric($value);
+    }
+
+    protected function validateMin($value, $params): bool
+    {
+        return strlen($value) >= $params[0];
+    }
+
+    protected function validateMimes($field, $value, $file, $params):bool
+    {
+        $allowedMimes = $params;
+
+        if (isset($file['type'])) {
+            $mime = $file['type'];
+
+            foreach ($allowedMimes as $allowedMime) {
+                if (strpos($mime, $allowedMime) === 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
