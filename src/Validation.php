a2e244654f31c297b2dd7df25c4f8a5fa85c0692
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-05-04 08:27:04 +0100

    Added new rules structure

--- src/Validation.php
+++ src/Validation.php
@@ -3,21 +3,25 @@
  * Validation class
  * @package validation
  * @version 1.0.0
- * @author Michael Arawole <michael@loogad,net>
- * @link https://www.logad.net
+ * @author Michael Arawole <michael@logad.net>
+ * @link https://apps.logad.net/logadapp/validation
  * @date 20 Apr, 2023 8:30 AM
  */
 
-namespace Logadapp\Validator;
+namespace LogadApp\Validator;
 
 use Exception;
 
 final class Validation
 {
-    public array $postData;
-    public array $files;
-    public array $rules;
+    private array $postData;
+
+    private array $files;
+
+    private array $rules;
+
     private array $errors = [];
+
     private array $errorMessages = [];
 
     public function getErrors(): array
@@ -93,12 +97,9 @@ final class Validation
         echo $methodName, PHP_EOL;
         echo PHP_EOL;
 
-        if (method_exists($this, $methodName)) {
-            $callback = [$this, $methodName];
-        } elseif (function_exists($methodName)) {
-            $callback = $methodName;
-        }
-
+        $ruleClass = new Rule($this->postData, $this->files);
+        $callback = [$ruleClass, $methodName];
+        print_r($callback);
         if (is_callable($callback)) {
             $validateResult = call_user_func($callback, $field, $value, $file, $params);
             if (!$validateResult['status']) {
@@ -107,155 +108,7 @@ final class Validation
             }
         } else {
             // log error
-            // throw new Exception("Validation rule not found: $rule");
+            throw new Exception("Validation rule not found: $rule");
         }
-    }
-
-    private function validateRequired(string $field, string $value): array
-    {
-        return [
-            'status' => !empty($value),
-            'message' => $field . ' - is required'
-        ];
-    }
-
-    private function validateRequiredIf(string $field, mixed $value, array $file, array $params): array
-    {
-        $requiredField = $params[0];
-        $status = false;
-
-        if (isset($this->postData[$requiredField])) {
-            $status = in_array($this->postData[$requiredField], array_slice($params, 1));
-        }
-
-        return [
-            'status' => !($status && empty($value)),
-            'message' => $field . ' -  is required'
-        ];
-    }
-
-    private function validateMin(string $field, int $value, array $file, array $params): array
-    {
-        return [
-            'status' => $value >= $params[0],
-            'message' => $field . ' - Min value must be '.$params[0]
-        ];
-    }
-
-    private function validateMax(string $field, int $value, array $file, array $params): array
-    {
-        return [
-            'status' => $value <= $params[0],
-            'message' => $field . ' - Max value must be '.$params[0]
-        ];
-    }
-
-    private function validateMinLength(string $field, string $value, array $file, array $params): array
-    {
-        return [
-            'status' => strlen($value) >= $params[0],
-            'message' => $field . ' - Min length must be '.$params[0]
-        ];
-    }
-
-    private function validateMaxLength(string $field, string $value, array $file, array $params): array
-    {
-        return [
-            'status' => strlen($value) <= $params[0],
-            'message' => $field . ' - Max length must be '.$params[0]
-        ];
-    }
-
-    private function validateMaxSize(string $field, string|array $value, array $file, array $params): array
-    {
-        $maxSize = (int) $params[0];
-        $status = false;
-
-        if (isset($file['size'])) {
-            $status = $file['size'] <= $maxSize;
-        }
-
-        if (is_string($value)) {
-            $status = strlen($value) <= $maxSize;
-        }
-
-        if (is_array($value)) {
-            $status = count($value) <= $maxSize;
-        }
-
-        return [
-            'status' => $status,
-            'message' => $field . ' - Max values is ' . $maxSize
-        ];
-    }
-
-    private function validateEmail(string $field, string $value): array
-    {
-        return [
-            'status' => (filter_var($value, FILTER_VALIDATE_EMAIL) !== false),
-            'message' => $field. ' - Invalid email format'
-        ];
-    }
-
-    private function validateNumeric(string $field, mixed $value): array
-    {
-        return [
-            'status' => is_numeric($value),
-            'message' => $field . ' -  is not numeric'
-        ];
-    }
-
-    private function validateIn(string $field, mixed $value, array $file, array $params): array
-    {
-        return [
-            'status' => in_array($value, $params),
-            'message' => $field . ' -  is not between ' . implode(', ', $params)
-        ];
-    }
-
-    private function validateFile(string $field, mixed $value, array $file, array $params): array
-    {
-        return [
-            'status' => !empty($file['name']),
-            'message' => $field . ' -  is not a file'
-        ];
-    }
-
-    private function validateFileSize(string $field, mixed $value, array $file, array $params): array
-    {
-        $maxSize = (int) $params[0];
-        $status = false;
-
-        if (isset($file['size'])) {
-            $status = $file['size'] <= $maxSize;
-        }
-
-        return [
-            'status' => $status,
-            'message' => $field . ' - Max file size is ' . $maxSize
-        ];
-    }
-
-    private function validateFileType(string $field, mixed $value, array $file, array $params): array
-    {
-        $allowedMimes = $params;
-        print_r($allowedMimes);
-        echo 'File';
-        print_r($file);
-        $status = false;
-        
-        if (isset($file['type'])) {
-            $mime = $file['type'];
-            foreach ($allowedMimes as $allowedMime) {
-                if (str_starts_with($mime, $allowedMime)) {
-                    $status = true;
-                }
-            }
-        }
-
-        return [
-            'status' => $status,
-            'message' => $field . ' - Allowed file types are: ' . implode(', ', $allowedMimes)
-        ];
     }
 }
