9722a7ff092e62ab5d3d90b326c2a33435556cd8
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-07-03 23:01:07 +0100

    Added subRule for file validation

--- src/Rules/File.php
+++ src/Rules/File.php
@@ -17,9 +17,48 @@ final class File extends Rule
 {
     public function validate(string $field, mixed $value, array $file, array $params): array
     {
+
+        // Check if upload is okay first
+        $uploadIsOkay = (!empty($file['name']) && $file['error'] === UPLOAD_ERR_OK);
+        if (!$uploadIsOkay) {
+            return [
+                'status' => false,
+                'message' => 'is not a file'
+            ];
+        }
+
+        if (!empty($params)) {
+            // Min size
+            if (isset($params[0]) && isset($params[1])) {
+                $subRule = $this->callSubRule('fileSize', $field, $value, $file, [
+                    $params[0],
+                    $params[1]
+                ]);
+                if (!$subRule['status']) {
+                    return [
+                        'status' => false,
+                        'message' => $subRule['message']
+                    ];
+                }
+            }
+
+            if (isset($params[3])) {
+                $subRule = $this->callSubRule('fileType', $field, $value, $file, [
+                    $params[0],
+                    $params[1]
+                ]);
+                if (!$subRule['status']) {
+                    return [
+                        'status' => false,
+                        'message' => $subRule['message']
+                    ];
+                }
+            }
+        }
+
         return [
-            'status' => (!empty($file['name']) && $file['error'] === UPLOAD_ERR_OK),
-            'message' => 'is not a file'
+            'status' => false,
+            'message' => 'Incorrect file rule'
         ];
     }
 }
